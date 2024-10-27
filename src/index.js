03b65e0db1eeb06323306c06c121fcfa114e3ab0
Rodrigo Amaral <amaralrodrigo798@gmail.com>
2024-10-27 17:01:39 +0000

    Added footer and started implementing CSS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { pageLoad } from "./initialPageLoad";
+import { pageLoad, button1, button2, button3 } from "./initialPageLoad";
 import { loadContact } from "./contact";
 import { loadHome } from "./home";
 import { loadMenu } from "./menu";
 
 pageLoad();
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
 
 button1.addEventListener("click", loadHome);
 button2.addEventListener("click", loadContact);
--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -3,21 +3,21 @@ import restaurantImage from "./restaurant.jpg";
 const image = document.createElement("img");
 image.src = restaurantImage;
 
-const header = document.querySelector("header");
-
 const nav = document.querySelector("nav");
 
 const contentDiv = document.querySelector("#content");
 
-const button1 = document.createElement("button");
+const footer = document.querySelector("footer");
+
+export const button1 = document.createElement("button");
 button1.id = "button1";
 button1.innerHTML = "Home";
 
-const button2 = document.createElement("button");
+export const button2 = document.createElement("button");
 button2.id = "button2";
 button2.innerHTML = "Menu";
 
-const button3 = document.createElement("button");
+export const button3 = document.createElement("button");
 button3.id = "button3";
 button3.innerHTML = "Contact";
 
@@ -27,4 +27,13 @@ export function pageLoad() {
     nav.appendChild(button3);
 
     contentDiv.appendChild(image);
+
+    const footerH1 = document.createElement("h1");
+    footerH1.textContent = "FOGUETEIRO";
+
+    const footerContent = document.createElement("p");
+    footerContent.textContent = "The portuguese traditional restaurant.";
+
+    footer.appendChild(footerH1);
+    footer.appendChild(footerContent);
 }
